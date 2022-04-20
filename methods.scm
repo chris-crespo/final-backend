b97c9ac0b46c5e55d12fdcacff896e412c786f1f
krs98 <christian.crespoo98@gmail.com>
2022-04-20 19:27:10 +0200

    try fix

--- methods.scm
+++ methods.scm
@@ -5,7 +5,7 @@
   (import scheme)
   (cond-expand 
     (chicken-4
-      (import chicken extras)
+      (import chicken (except extras get))
       (use awful spiffy spiffy-request-vars
            medea intarweb))
     (chicken-5
